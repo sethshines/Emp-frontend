34421f5eb65b4600fa066e01ef7f681abfd3c941
Rahul Seth <sethshines@gmail.com>
2024-12-22 12:54:10 -0600

    feat: [EMP-002] Boilerplate Code Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h6 className='text-3xl'>Employee Management App</h6>
+    </div>
+  );
+}
+
+export default App;
